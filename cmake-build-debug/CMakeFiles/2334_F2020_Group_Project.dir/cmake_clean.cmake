2af6949c4ad2095159080a50294e866b69dfaf8e
Zachary Steudel <zacshack174@gmail.com>
2020-08-19 19:33:15 -0500

    made the project

---
+++ cmake-build-debug/CMakeFiles/2334_F2020_Group_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2334_F2020_Group_Project.exe"
+  "2334_F2020_Group_Project.exe.manifest"
+  "2334_F2020_Group_Project.pdb"
+  "CMakeFiles/2334_F2020_Group_Project.dir/main.c.obj"
+  "lib2334_F2020_Group_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/2334_F2020_Group_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
