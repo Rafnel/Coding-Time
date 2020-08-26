6e88b2589e10138c245f6c6c4abc2546bbba43d9
Joshua Craig Hunter <joshua_hunter1@baylor.edu>
2020-08-25 23:15:23 -0500

    Updated the main with the basic implementation of the calculator as well as the if statements that branch off to the seperate opperations

--- main.c
+++ main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
 
-    printf("Howdy brother\n");
+    int Choice = -1;
+
+    do {
+
+        printf("1. Add\n"
+               "2. Subtract\n"
+               "3. Multiply\n"
+               "4. Divide\n"
+               "5. Power\n"
+               "6. Log\n"
+               "0. Exit\n");
+
+        printf("Select the operation you would like to perform!\n");
+        scanf("%d", &Choice);
+        printf("Choice = %d\n", Choice);
+
+        if (Choice == 1) {
+
+        } else if (Choice == 2) {
+
+        } else if (Choice == 3) {
+
+        } else if (Choice == 4) {
+
+        } else if (Choice == 5) {
+
+        } else if (Choice == 6) {
+
+        }
+    } while(Choice != 0);
+
     return 0;
 }
